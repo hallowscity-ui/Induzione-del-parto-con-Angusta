2dbdb828288f599b55f5441c84c56b9bae53ed1f
hallowscity-ui <hallowscity@gmail.com>
2026-01-09 22:04:15 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = "induzione-cache-v1";
+const filesToCache = [
+  "./index.html",
+  "./sw.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+});
+
+self.addEventListener("activate", e => { e.waitUntil(self.clients.claim()); });
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
